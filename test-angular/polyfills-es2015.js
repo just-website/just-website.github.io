320758a195d2aed90577c75e87be7672f8887fcb
Gleb Shchukin <just-website@yandex.ru>
2019-10-05 00:57:06 +0300

    rename folder and rebuild project

--- test-angular/polyfills-es2015.js
+++ test-angular/polyfills-es2015.js
@@ -5982,8 +5982,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\Users\Gleb\Learn\Angular\VRt-test-task\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! c:\Users\Gleb\Learn\Angular\VRt-test-task\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Gleb\Learn\Angular\VRt-test-task\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Gleb\Learn\Angular\VRt-test-task\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
